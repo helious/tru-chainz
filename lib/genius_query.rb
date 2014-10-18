3733370c249826c1e55b7a9848c58dd94f98383d
Uy Nguyen <nguyenuy@gmail.com>
2014-10-18 16:23:26 -0400

    Initial implementation of genius_query.rb

---
+++ lib/genius_query.rb
@@ -0,0 +1,27 @@
+require 'rapgenius'
+
+class RapGenius
+    def initialize
+    end
+
+    #Return Genius ID of song, if not found, return -1
+    def get_genius_result_id artist, song
+        rap_query_array = RapGenius.search_by_title(song)
+
+        #Find matching artist
+        id = -1
+        rap_query_array.each do | entry |
+            if entry.artist.name.casecmp(artist) == 0
+                id = entry.id
+                break
+            end
+        end
+        return id
+    end
+
+    #Get the song object, which contains lines and annotations
+    def get_genius_song id
+        song = RapGenius::Song.find(id)
+    end
+
+end
